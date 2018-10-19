e7dfe7aa114988b1e52dcb8602e4f079f819c303
Aaron Adams <aaronadams19@gmail.com>
2018-10-18 18:46:41 -0600

    more changes

--- java/examples/shapes/examples/Testing/FactoryTesting/ShapeFactoryTest.java
+++ java/examples/shapes/examples/Testing/FactoryTesting/ShapeFactoryTest.java
@@ -2,11 +2,12 @@ package examples.Testing.FactoryTesting;
 
 import static org.junit.Assert.*;
 
-
+import java.util.ArrayList;
 
 import org.junit.Test;
 
-import Factory.ShapeFactory;
+import examples.Composite.ShapeComponent;
+import examples.Factory.ShapeFactory;
 import examples.shapes.Circle;
 import examples.shapes.Ellipse;
 import examples.shapes.Rectangle;
@@ -20,15 +21,24 @@ public class ShapeFactoryTest {
 	@Test
     public void testCreateCircle() throws ShapeException   {
         
-		Circle circle = new Circle(0,0,5);
+		Circle circle = new Circle(0.0,0.0,5.0);
 		 
 		String input = "Circle";
-		int circleArray[] = null;
-		circleArray[0] = 0;
-		circleArray[1] = 0;
-		circleArray[2] = 5;
+		ArrayList<Double> circleArray = new ArrayList();
+		circleArray.add(0.0);
+		circleArray.add(0.0);
+		circleArray.add(5.0);
+		
 		ShapeFactory factory = new ShapeFactory();	
-		assertSame(circle, factory.makeShape(input, circleArray)); 
+		
+		ShapeComponent newCircle = factory.makeShape(input, circleArray);
+		
+		ArrayList<Double> parameters = newCircle.getAllparameters();
+		
+		
+		assertEquals(circleArray.get(0),parameters.get(0));
+		assertEquals(circleArray.get(1),parameters.get(1));
+		assertEquals(circleArray.get(2),parameters.get(2));
     }
 
 	
@@ -41,13 +51,24 @@ public class ShapeFactoryTest {
 		Ellipse ellipse = new Ellipse(0,0,5, 10);
 		 
 		String input = "Ellipse";
-		int ellipseArray[] = null;
-		ellipseArray[0] = 0;
-		ellipseArray[1] = 0;
-		ellipseArray[2] = 5;
-		ellipseArray[3] = 10;
+		
+		ArrayList<Double> ellipseArray = new ArrayList();
+		ellipseArray.add(0.0);
+		ellipseArray.add(0.0);
+		ellipseArray.add(5.0);
+		ellipseArray.add(10.0);
+		
 		ShapeFactory factory = new ShapeFactory();	
-		assertSame(ellipse, factory.makeShape(input, ellipseArray)); 
+		
+		ShapeComponent newEllipse = factory.makeShape(input, ellipseArray);
+		
+		ArrayList<Double> parameters = newEllipse.getAllparameters();
+		
+		
+		assertEquals(ellipseArray.get(0),parameters.get(0));
+		assertEquals(ellipseArray.get(1),parameters.get(1));
+		assertEquals(ellipseArray.get(2),parameters.get(2));
+		assertEquals(ellipseArray.get(3),parameters.get(3));
     }
 	
 	
@@ -58,12 +79,25 @@ public class ShapeFactoryTest {
 		Square square = new Square(0,0,5);
 		 
 		String input = "Square";
-		int squareArray[] = null;
-		squareArray[0] = 0;
-		squareArray[1] = 0;
-		squareArray[2] = 5;
+		
+		ArrayList<Double> squareArray = new ArrayList();
+		squareArray.add(0.0);
+		squareArray.add(0.0);
+		squareArray.add(5.0);
+		squareArray.add(10.0);
+		
 		ShapeFactory factory = new ShapeFactory();	
-		assertSame(square, factory.makeShape(input, squareArray)); 
+		
+		ShapeComponent newSquare = factory.makeShape(input, squareArray);
+		
+		ArrayList<Double> parameters = newSquare.getAllparameters();
+		
+		
+		assertEquals(squareArray.get(0),parameters.get(0));
+		assertEquals(squareArray.get(1),parameters.get(1));
+		assertEquals(squareArray.get(2),parameters.get(2));
+		assertEquals(squareArray.get(3),parameters.get(3));
+		
     }
 	
 	
@@ -74,13 +108,24 @@ public class ShapeFactoryTest {
 		Rectangle rectangle = new Rectangle(0,0,5, 10);
 		 
 		String input = "Rectangle";
-		int recArray[] = null;
-		recArray[0] = 0;
-		recArray[1] = 0;
-		recArray[2] = 5;
-		recArray[3] = 10;
+		
+		ArrayList<Double> recArray = new ArrayList();
+		recArray.add(0.0);
+		recArray.add(0.0);
+		recArray.add(5.0);
+		recArray.add(10.0);
+		
 		ShapeFactory factory = new ShapeFactory();	
-		assertSame(rectangle, factory.makeShape(input, recArray)); 
+		
+		ShapeComponent newRec = factory.makeShape(input, recArray);
+		
+		ArrayList<Double> parameters = newRec.getAllparameters();
+		
+		
+		assertEquals(recArray.get(0),parameters.get(0));
+		assertEquals(recArray.get(1),parameters.get(1));
+		assertEquals(recArray.get(2),parameters.get(2));
+		assertEquals(recArray.get(3),parameters.get(3));	
     }
 	
 	@SuppressWarnings("null")
@@ -89,18 +134,34 @@ public class ShapeFactoryTest {
         
 		Triangle triangle = new Triangle(0,0,10,10,5,5,10,5);
 		 
-		String input = "Rectangle";
-		int triArray[] = null;
-		triArray[0] = 0;
-		triArray[1] = 0;
-		triArray[2] = 10;
-		triArray[3] = 10;
-		triArray[4] = 5;
-		triArray[5] = 5;
-		triArray[6] = 10;
-		triArray[7] = 5;
+		String input = "Triangle";
+		ArrayList<Double> triArray = new ArrayList();
+		triArray.add(1.3);
+		triArray.add(2.6);
+		triArray.add(10.0);
+		triArray.add(15.0);
+		triArray.add(5.0);
+		triArray.add(5.0);
+		triArray.add(15.0);
+		triArray.add(5.0);
+		
+		//1.3, 2.6, 10, 15, 5, 5, 15, 5
+		
 		ShapeFactory factory = new ShapeFactory();	
-		assertSame(triangle, factory.makeShape(input, triArray)); 
+		
+		ShapeComponent newRec = factory.makeShape(input, triArray);
+		
+		ArrayList<Double> parameters = newRec.getAllparameters();
+		
+		
+		assertEquals(triArray.get(0),parameters.get(0));
+		assertEquals(triArray.get(1),parameters.get(1));
+		assertEquals(triArray.get(2),parameters.get(2));
+		assertEquals(triArray.get(3),parameters.get(3));
+		assertEquals(triArray.get(4),parameters.get(4));
+		assertEquals(triArray.get(5),parameters.get(5));
+		assertEquals(triArray.get(6),parameters.get(6));
+		assertEquals(triArray.get(7),parameters.get(7));
     }
 	
 	
